c29a37130fdb7387d92fc203e55e81dcf67d0980
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-11-06 16:51:49 +0100

    Jour 5

--- javascript-langage/ex-jeu.js
+++ javascript-langage/ex-jeu.js
@@ -1,3 +1,23 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // importe l'API readline de Node.js
 // doc: https://nodejs.org/dist/latest-v14.x/docs/api/readline.html
 const readline = require('readline');
@@ -8,16 +28,36 @@ const rl = readline.createInterface({
   output: process.stdout, // écrire dans le terminal
 });
 
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
+
 function jouer() {
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(' - '));
+  }
+
   rl.question('Quel est le nombre ? ', (answer) => {
     console.log('Vous avez saisi : ' + answer);
 
-    // rejouer
-    jouer();
+    const entierSaisi = Number.parseInt(answer, 10);
 
-    // si le nombre a été trouvé :
-    // console.log('Gagné');
-    // rl.close();
+    if (Number.isNaN(entierSaisi)) {
+      console.log('Erreur : il faut saisir un entier');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
   });
 }
 
